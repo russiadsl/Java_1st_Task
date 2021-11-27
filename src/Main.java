611a9ea94bb0778c20c217e3f1cdeff40e9d3f97
Evgeny Bezugly <bezugly_spb@mail.ru>
2021-11-27 10:27:04 +0300

    Added bankink app

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int currentBalance = 2_000_000_000;
+        int transferAmmount = 500_000_000;
+        int total = currentBalance + transferAmmount;
+
+        System.out.println(total);
+    }
+}
